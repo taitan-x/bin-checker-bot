6e09e412756a346a94dce938adf929a9356a2cbc
taitan-x <138150134+taitan-x@users.noreply.github.com>
2023-08-23 10:06:13 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -63,7 +63,7 @@ else {
 }
     function send_message($chat_id,$message_id, $message){
         $text = urlencode($message);
-        $apiToken = $_ENV['API_TOKEN'];  
+        $apiToken = $_ENV['s1UwoOUn7h54ZLuXRWlQnTjg9btkKAQp'];  
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text&parse_mode=Markdown");
     }
 ?>
